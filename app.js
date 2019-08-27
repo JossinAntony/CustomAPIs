8b6e93f00aa6bc24aee45238df4b8602f0ae9f22
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-27 15:47:36 +0530

    procfile added, dbconnect & apilinks changed to heroku links

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 var app = new Express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 // For CORS,Pgm Line no 12 to 29
@@ -28,8 +29,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-Mongoose.connect('mongodb://localhost:27017/CustomDB');
-//Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
+//Mongoose.connect('mongodb://localhost:27017/CustomDB');
+Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
 
 ////////////////////////////////////////////////
 //define dataschema fro sign-up data in S:\CausalityBiomodels\ABCD\ICT\HW\HW26082019_1_LogInForm\ProjectLogIn
@@ -62,63 +63,63 @@ app.get('/saveInfo/',(req,res)=>{
     });
 });
 
-////define retrievel API
-app.get('/retrieveInfo',(req,res)=>{
-    var retrieve = formSchema.find((error,data)=>{
-        if (error){
-            throw error;
-        }else{
-            res.send(data);
-        }
+// ////define retrievel API
+// app.get('/retrieveInfo',(req,res)=>{
+//     var retrieve = formSchema.find((error,data)=>{
+//         if (error){
+//             throw error;
+//         }else{
+//             res.send(data);
+//         }
         
-    });
-});
+//     });
+// });
 
-///get link to the retrievel API
-const retrieveDataApi = "http://localhost:3046/retrieveInfo"
-//const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
+// ///get link to the retrievel API
+// const retrieveDataApi = "http://localhost:3052/retrieveInfo"
+// //const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
 
-///call the API in a function to retieve the data
-app.get('/viewpersons',(req,res)=>{
-    request(retrieveDataApi,(error, response, body)=>{
-        if (error){
-            throw error;
-        }else{
-            var data= JSON.parse(body);
-            res.send(data);
-        }
-    });
-});
-/////////////////////////////////////////////////////
-//define the API to get a singleperson
+// ///call the API in a function to retieve the data
+// app.get('/viewpersons',(req,res)=>{
+//     request(retrieveDataApi,(error, response, body)=>{
+//         if (error){
+//             throw error;
+//         }else{
+//             var data= JSON.parse(body);
+//             res.send(data);
+//         }
+//     });
+// });
+// /////////////////////////////////////////////////////
+// //define the API to get a singleperson
 
-app.get('/searchByMobAPI/',(req,res)=>{
-    var prsnmob = req.query.q;
-    formSchema.find({umob:prsnmob}, (error, data)=>{
-        if(error){
-            throw error;
-        }else{
-            res.send(data);
-        }
-    });
-});
+// app.get('/searchByMobAPI/',(req,res)=>{
+//     var prsnmob = req.query.q;
+//     formSchema.find({umob:prsnmob}, (error, data)=>{
+//         if(error){
+//             throw error;
+//         }else{
+//             res.send(data);
+//         }
+//     });
+// });
 
-//define API link
-const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
-//const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
+// //define API link
+// const searchByNameAPILink = 'http://localhost:3052/searchByMobAPI'
+// //const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
 
-//use in function to retrieve data
-app.post('/searchPerson',(req,res)=>{
-    item = req.body.smob;
-    request(searchByNameAPILink+"/?q="+ item, (error, response, body)=>{
-        if(error){
-            throw error;
-        }else{
-            var data = JSON.parse(body);
-            res.send(data);
-        } 
-    });
-});
+// //use in function to retrieve data
+// app.post('/searchPerson',(req,res)=>{
+//     item = req.body.smob;
+//     request(searchByNameAPILink+"/?q="+ item, (error, response, body)=>{
+//         if(error){
+//             throw error;
+//         }else{
+//             var data = JSON.parse(body);
+//             res.send(data);
+//         } 
+//     });
+// });
 
 
 app.get('/',(req,res)=>{
