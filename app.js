fa85419406a05e1c9fead005008a907eed047ddb
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-29 11:08:53 +0530

    heroku link edit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
   //  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
-    res.setHeader('Access-Control-Allow-Origin', 'https://customapi-jossin.herokuapp.com' );
+    res.setHeader('Access-Control-Allow-Origin', 'https://studentdb1-jossin.herokuapp.com' );
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
